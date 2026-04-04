95069acbdf432a3117fb79d5917b84a1271e1c9e
Mario Reyes <ernestoreyes561@gmail.com>
2026-04-04 01:39:28 -0600

    Eliminación de código no usado.

--- 2026/Day3/tenencia_vivienda.R
+++ 2026/Day3/tenencia_vivienda.R
@@ -3,12 +3,6 @@ library(ggmosaic)
 
 vivienda <- read.csv("../../../censo_2024/Bases-Finales-CPV2024SV-CSV/Base de Datos de Viviendas - CPV 2024 SV.csv")
 
-tenencia <- vivienda |> 
-  group_by(V07_VIV_TENENCIA) |> 
-  summarise(n = n(), .groups = "drop") |> 
-  drop_na() |>
-  mutate(prop = round(n / sum(n)*100, 0))
-
 vivienda_mp <- vivienda %>%
   mutate(
     tenencia = factor(V07_VIV_TENENCIA),
@@ -27,15 +21,3 @@ ggplot(vivienda_mp) +
 
 
 
-# 1. Create the base mosaic plot
-p <- ggplot(data = vivienda_mp) +
-  geom_mosaic(aes(x = product(tenencia, area), fill = tenencia))
-
-# 2. Extract calculated data to add percentage labels
-plot_data <- ggplot_build(p)$data[[1]]
-
-# 3. Add labels (example uses .wt which is the count)
-p + geom_text(data = plot_data, 
-              aes(x = (xmin + xmax) / 2, 
-                  y = (ymin + ymax) / 2, 
-                  label = scales::percent(prop))) # 'prop' is often the cell proportion
